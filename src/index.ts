664b2ddd1cbcdf2f4e851dd951b66b4fc6b169cd
bosh-code <ryan.julian.bosher@icloud.com>
2021-06-13 10:21:50 +1200

    update index prints and fix some readability issues

--- src/index.ts
+++ src/index.ts
@@ -13,39 +13,43 @@ const createArr = (): number[] => {
 const verbose = false
 
 console.log(chalk.bold(chalk.blue('Running sorts!')))
-console.log(chalk.italic(' - Verbose output is currently', chalk.yellow(verbose), '\n', '- The default for all methods is', chalk.yellow(false), '\n', '- Output from merge sorting is', chalk.bold('huge,'), 'pass', chalk.yellow(true), 'to the function call on line 38 at your own risk!\n'))
 
 const bubbleArr = createArr()
 console.log(chalk.blue('Bubble sort!'))
 console.log('Numbers to sort: ', chalk.red(bubbleArr))
 console.log('Numbers after bubble sort: ', chalk.green(bubbleSort(bubbleArr, verbose)))
 console.log(chalk.bold('Total number of operations:', chalk.green(bubbleCount)))
-console.log(chalk.italic('Bubble sorting is not very efficient, with an average and worst case big O notation of: O(n2)\n'))
+console.log(chalk.italic('Bubble sorting is not very efficient, with an average and worst case big O notation of: O(n2).\n'))
 
 const selectionArr = createArr()
 console.log(chalk.blue('Selection sort!'))
 console.log('Numbers to sort: ', chalk.red(selectionArr))
 console.log('Numbers after selection sort: ', chalk.green(selectionSort(selectionArr, verbose)))
 console.log(chalk.bold('Total number of operations:', chalk.green(selectionCount)))
-console.log(chalk.italic('Selection sorting is also very inefficient with an average big O notation of: O(n2)\n'))
+console.log(chalk.italic('Selection sorting is also very inefficient with an average big O notation of: O(n2).\n'))
 
 const insertionArr = createArr()
 console.log(chalk.blue('Insertion sort!'))
 console.log('Numbers to sort: ', chalk.red(insertionArr))
 console.log('Numbers after insertion sort: ', chalk.green(insertionSort(insertionArr, verbose)))
 console.log(chalk.bold('Total number of operations:', chalk.green(insertionCount)))
-console.log(chalk.italic('Insertion sorting is simple and effective for small arrays, however has a worst case scenario of O(n2)\n'))
+console.log(chalk.italic('Insertion sorting is simple and effective for small arrays, however has a worst case scenario of O(n2).\n'))
 
 const mergeArr = createArr()
 console.log(chalk.blue('Merge sort!'))
 console.log('Numbers to sort: ', chalk.red(mergeArr))
 console.log('Numbers after merge sort: ', chalk.green(mergeSort(mergeArr))) // Uses default false. Pass true if you want.
 console.log(chalk.bold('Total number of operations:', chalk.green(mergeCount)))
-console.log(chalk.italic('...\n'))
+console.log(chalk.italic('Merge sorting is a comparison sorting algorithm that runs in O( n log n) time.\n'))
 
 const quickArr = createArr()
 console.log(chalk.blue('Quick sort!'))
 console.log('Numbers to sort: ', chalk.red(quickArr))
 console.log('Numbers after quick sort: ', chalk.green(quickSort(quickArr, 0, quickArr.length - 1, verbose)))
 console.log(chalk.bold('Total number of operations:', chalk.green(quickCount)))
-console.log(chalk.italic('...O(n log n)\n'))
+console.log(chalk.italic('Quick sorting is very popular and frequently used. It runs in O(n log n) time.\n'))
+
+console.log(chalk.italic(' - Verbose output is currently', chalk.yellow(verbose)))
+console.log(chalk.italic(' - The default for all methods is', chalk.yellow(false)))
+console.log(chalk.italic(' - Output from merge sorting is', chalk.bold('huge,'), 'pass', chalk.yellow(true), 'to the function call on line 41 at your own risk!'))
+console.log(chalk.italic(' - The total number of operations displayed for each sort is', chalk.bold('not'), 'an indication of performance, just an interesting metric.'))
--- src/mergeSort.ts
+++ src/mergeSort.ts
@@ -6,39 +6,30 @@ export function mergeSort (arr: number[], verbose = false): number[] {
   if (arr.length < 2) {
     return arr
   }
-
   const midPoint = Math.floor(arr.length / 2)
   const leftArr = arr.slice(0, midPoint)
   const rightArr = arr.slice(midPoint)
-
   if (verbose) console.log(chalk.grey(`Slicing array: ${arr} at: ${arr[midPoint]}, leftArr: ${leftArr} | rightArr: ${rightArr}`))
-
   return merge(mergeSort(leftArr, verbose), mergeSort(rightArr, verbose), verbose)
 }
 
 function merge (leftArr: number[], rightArr: number[], verbose = false): number[] {
   if (verbose) console.log(chalk.grey(`Merging arrays: ${leftArr} and: ${rightArr}`))
-
   const result = []
-
   let lIndex = 0
   let rIndex = 0
+
   while (lIndex < leftArr.length && rIndex < rightArr.length) {
     if (leftArr[lIndex] < rightArr[rIndex]) {
       result.push(leftArr[lIndex])
-
       if (verbose) console.log('Pushing:', chalk.yellow(leftArr[lIndex]), 'from leftArr:', chalk.yellow(result))
-
       lIndex++
-      ++swapCount
     } else {
       result.push(rightArr[rIndex])
-
       if (verbose) console.log('Pushing:', chalk.yellow(rightArr[rIndex]), 'from rightArr:', chalk.yellow(result))
-
       rIndex++
-      ++swapCount
     }
+    ++swapCount
   }
   return result.concat(leftArr.slice(lIndex)).concat(rightArr.slice(rIndex))
 }
